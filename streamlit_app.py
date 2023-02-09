f962473953f7b2c20a4f1adb0ce80552262c803f
Jacob Kline <74945268+JacobKline18@users.noreply.github.com>
2023-02-09 13:33:55 -0600

    Update streamlit_app.py
    import pandas, call remote csv file to populate food dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas;
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,3 +12,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_marcos.txt")
+
+streamlit.dataframe(my_fruits_list)
