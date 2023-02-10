6c19e50794c7d50aa8786b85fbfcaab0337e85f7
Jacob Kline <74945268+JacobKline18@users.noreply.github.com>
2023-02-10 14:11:12 -0600

    Update streamlit_app.py
    turning response into json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,4 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) <--- grabbing response body
+streamlit.text(fruityvice_response.json())# <--- grabbing response body
