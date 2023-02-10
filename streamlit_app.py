30bf34fbd2a957c4e619f12ada0f96bf1495ecbd
Jacob Kline <74945268+JacobKline18@users.noreply.github.com>
2023-02-10 13:59:57 -0600

    Update streamlit_app.py
    added a GET request and checking response in text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas;
-
+import requests;
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -22,4 +22,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
