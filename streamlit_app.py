0ff2a2679c702c07845263ac6abef3a1a91488f6
Jacob Kline <74945268+JacobKline18@users.noreply.github.com>
2023-02-12 13:46:47 -0600

    Update streamlit_app.py
    added button for inserting custom fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,11 @@ def get_fruit_load_list():
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
+        return "Thanks for adding " + new_fruit
+    
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
   if not fruit_choice:
@@ -63,6 +68,9 @@ streamlit.stop
 
 # Allow an end-user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for entering ', add_my_fruit)
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
